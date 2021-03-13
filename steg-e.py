18994438028e0b44d5bb0117b53fc71027050ad8
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-03-14 00:17:51 +0530

    medium encryption implemented and tested ✔

--- steg-e.py
+++ steg-e.py
@@ -3,11 +3,13 @@ import getpass
 import os
 import sys
 import time
-from modules.Easy.EasyEncrypt import easyMethod_Rotate
-from modules.Easy.EasyDecrypt import easyMethod_Decrypt
-from modules.Intro.introScreen import key_graphics
-from modules.Intro.introScreen import lock
-from modules.Intro.introScreen import banner
+from pyModules.Easy.EasyEncrypt import easyMethod_Rotate
+from pyModules.Easy.EasyDecrypt import easyMethod_Decrypt
+from pyModules.Medium.MediumEncrypt import MediumEncrpyt_Method
+from pyModules.Medium.MediumDecrypt import MediumDecrypt_Method
+from pyModules.Intro.introScreen import key_graphics
+from pyModules.Intro.introScreen import lock
+from pyModules.Intro.introScreen import banner
 import re
 import colorama
 from colorama import Fore, Style
@@ -99,40 +101,53 @@ class Encryption(StegE):
         # the encrypted text will then be hidden into the src file and save it in tgt location
 
     def calculations(self):
-        
-        try:
-            with open(self.msg, "r") as mm:
-                mesg = mm.read()
-        except:
-            print(Fore.RED+"Unable to read message file!!!")
-            exit()
-
-        if(len(mesg)<10):
-            msg_len = "000"+str(len(mesg))
-        elif(len(mesg)<100):
-            msg_len = "00"+str(len(mesg))
-        elif(len(mesg)<1000):
-            msg_len = "0"+str(len(mesg))
-        else:
-            print("Message length is exceeding the limit of 999 characters, it will automatically sliced to first 999 characters.")
-            ch = input("Would you like to continue? [Y/N]: ")
-            if(ch=='N' or ch=='n'):
-                print(Fore.RED+"Program Terminated Successfully. You can try again..".center(100))
-                exit()
-            mesg = mesg[0:1000]
-            msg_len = "0999"
-        
-        salt = "{"+msg_len+self.level_of_encryption+"}" 
-        final_msg = ""
 
         if(int(self.level_of_encryption)==1):
+            try:
+                with open(self.msg, "r") as mm:
+                    mesg = mm.read()
+            except:
+                print(Fore.RED+"Unable to read message file!!!")
+                exit()
+
+            if(len(mesg)<10):
+                msg_len = "000"+str(len(mesg))
+            elif(len(mesg)<100):
+                msg_len = "00"+str(len(mesg))
+            elif(len(mesg)<1000):
+                msg_len = "0"+str(len(mesg))
+            else:
+                print("Message length is exceeding the limit of 999 characters, it will automatically sliced to first 999 characters.")
+                ch = input("Would you like to continue? [Y/N]: ")
+                if(ch=='N' or ch=='n'):
+                    print(Fore.RED+"Program Terminated Successfully. You can try again..".center(100))
+                    exit()
+                mesg = mesg[0:1000]
+                msg_len = "0999"
+            
+            salt = "{"+msg_len+self.level_of_encryption+"}" 
+            final_msg = ""
             final_msg = easyMethod_Rotate(mesg, self.pwd)
+            
+            return salt+final_msg+salt  
+
         elif(int(self.level_of_encryption)==2):
-            pass
+            try:
+                with open(self.msg, "r") as coco:
+                    cocoa = coco.read()
+            except:
+                print(Fore.RED+"Unable to read message file!!!")
+                exit()
+
+            ecr_msg = MediumEncrpyt_Method(cocoa, self.pwd)
+
+            salt = "{"+str(len(ecr_msg))+self.level_of_encryption+"}"
+        
+            return salt+ecr_msg+salt
+
         elif(int(self.level_of_encryption)==3):
             pass
 
-        return salt+final_msg+salt
 
 class Decryption(StegE):
     def __init__(self):
@@ -165,6 +180,7 @@ class Decryption(StegE):
         print("\nMessage here:\n"+Fore.CYAN+message_to_display)
 
     def calculations(self):
+
         try:
             with open(self.tgt, "rb") as file:
                 encrypted_source_msg = file.read()
@@ -174,17 +190,32 @@ class Decryption(StegE):
             exit()
 
 
-        encrypted_source_msg = re.findall(r"\{[0-9]{1,5}\}.*\{[0-9]{1,5}\}$".encode('utf-8'), encrypted_source_msg)
-        length_of_message = int(encrypted_source_msg[0].decode('utf-8')[1:5])
-        level_of_encryption = int(encrypted_source_msg[0].decode('utf-8')[5])
-
-        tulsi = input("Enter the password to Decrypt: ")
-
+        encrypted_source_msg_temp = encrypted_source_msg
+        encrypted_source_msg_temp = re.findall(r"\{[0-9]{2,}\}.*\{[0-9]{2,}\}$".encode('utf-8'), encrypted_source_msg)
+        level_of_encryption = int(encrypted_source_msg_temp[0].decode('utf-8')[-2])
 
         if(level_of_encryption == 1):
+            
+            encrypted_source_msg = re.findall(r"\{[0-9]{1,5}\}.*\{[0-9]{1,5}\}$".encode('utf-8'), encrypted_source_msg)
+            length_of_message = int(encrypted_source_msg[0].decode('utf-8')[1:5])
+            level_of_encryption = int(encrypted_source_msg[0].decode('utf-8')[5])
+
+            tulsi = input("Enter the password to Decrypt: ")
+            
             return easyMethod_Decrypt(encrypted_source_msg[0][7:7+length_of_message].decode('utf-8'), tulsi)
+
         elif(level_of_encryption == 2):
-            pass
+
+            encrypted_source_msg = re.findall(r"2\}.*\{".encode('utf-8'), encrypted_source_msg)
+            encrypted_source_msg = encrypted_source_msg[-1].decode('utf-8')[2:-1]
+
+            tulsi = input("Enter the password to Decrypt: ")
+            
+            try:
+                return MediumDecrypt_Method(encrypted_source_msg, tulsi)
+            except:
+                return "Incorrect Key"
+
         elif(level_of_encryption == 3):
             pass
 
