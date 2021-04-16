1c6519a8fdf3a7e90d7cb3690d2921cd9570c027
Ashish Chaubey <ashish.chaubey1994@gmail.com>
2021-04-16 23:22:56 +0530

    decryt packt

--- networking/newpacket.py
+++ networking/newpacket.py
@@ -1,40 +1,33 @@
 import sys
+import gibberish
 from scapy.all import *
+from scapy.utils import PcapWriter
 from scapy.arch import windows
+from gibberish import Gibberish
 
-a="""vousPXab9rd4VajKBPml@#
-1qyInYhgxLaDhu8YFApT!4
-p6MJYZUx2UHWvqqvXwXa1~3
-AXY665wOHyWjsRF4AE5B&%
-JSVrcKubMq7cWOF7k7ci:
-zyCEzR2XkfdH7ZQg0lEN
-HYJ0GdLGRkeo701Tl2RO
-RcT0EPv2B0QyS2mDtTlJ
-xYbvcYtKKHQEjHociOpV
-VsC84NoMcw260MGwRa0N
-0NDFttHzcX9aDVL7YPsA
-rgt2K82RaYT4KVKFUbW9
-Y3d6TDCDJdBQM2oQQgAB
-73q2vXdPGS5D2xsasySj
-EEdzosTi98DsTGsJm06j
-fxfhhBvA46ZH5pHM4GtX
-lpmvGScqeaH7v0JxRkip
-yfBQ899d5PiXiruLLCAf
-86guJXm81z2cUhi88Pae
-17VgOw4vnQhSZsZkDjmT
-se6ZBx2WPNa4dUOTezcy
-SYNFrgVpfo1J3sTenRup
-P8pI2LxqVsEXnxnC9w0O
-FW03fDLKvaBr1GXAQtxP
-732yEmqf7Bfi832oREYf"""
+pktdump = PcapWriter("Test5.pcap", append=True, sync=True)
 
-asffghhhhhhhhhhhfgl
-
-pck=Ether()/IP(src="127.0.0.1",dst="127.0.0.1")/TCP(window=1234)/a
-print("showing packet")
+key="Tst@!@#$Tsdkjfksdfdsgldglfgkjlkajlskjdas"
+message="holla I am te encrypted message"
 
 
-wrpcap('new3.pcap',pck)
+key_array=[]
+gib=Gibberish()
+for char in key:
+    p=(ord(char)*80)
+    key_array.append(p)
+
+for int_key in key_array:
+    rawword=gib.generate_word(12)
+    print(len(rawword))
+    pck1=Ether()/IP(src="127.0.0.1",dst="127.0.0.1")/TCP(window=int_key)/rawword
+    pktdump.write(pck1)
+
+pck2=Ether()/IP(src="127.0.0.1",dst="127.0.0.1")/TCP()/message
+pktdump.write(pck2)
+
+#print("pcap file made")
+
 
 #p=rdpcap('my.pcap')
 #k=len(p)
