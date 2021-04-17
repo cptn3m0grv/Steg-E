f42eef4d76b0fed31df562cc841caf5f640e5bee
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-04-17 12:56:04 +0530

    network encrypt and decrypt with about page

--- temp/about.py
+++ temp/about.py
@@ -1,14 +1,6 @@
 import random
 import cowsay
 
-# about_banner = [cowsay.cheese('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.daemon('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.dragon('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.kitty('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.meow('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.milk('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.pig('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.stegosaurus('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.turkey('Hello!!!, what`s up!!!, Heard you wanna know about us.'), cowsay.turtle('Hello!!!, what`s up!!!, Heard you wanna know about us.')]
-
-about_banner = [cowsay.cheese, cowsay.daemon, cowsay.dragon, cowsay.kitty, cowsay.meow, cowsay.milk, cowsay.pig, cowsay.stegosaurus, cowsay.turkey, cowsay.turtle]
-
-a = random.choice(about_banner)
-
-print(a('Hello!!!, what`s up!!!, Heard you wanna know about us.')) 
-
 def about_page():
 
     about_banner = [cowsay.cheese, cowsay.daemon, cowsay.dragon, cowsay.kitty, cowsay.meow, cowsay.milk, cowsay.pig, cowsay.stegosaurus, cowsay.turkey, cowsay.turtle]
